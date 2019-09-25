58fad5035c08e01c53e1dfa8a7061b4d6d6276aa
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2019-09-25 11:10:35 -0600

    First Commit

---
+++ ArrayList Challenge #2/src/ArrayListChallenge2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class ArrayListChallenge2
+	{
+
+		public static void main(String[] args)
+			{
+				ArrayList <String> dogs = new ArrayList <String>();
+					dogs.add("Border Collie");
+					dogs.add("Blue Healer");
+					dogs.add("Golden Reterver");
+					dogs.add("Gray Hound");
+					dogs.add("Jack Russle Terrior");
+					dogs.add("German Shepherd");
+					dogs.add("Old English Shepherd");
+					dogs.add("Bernese Mountain Dog");
+					dogs.add("Corgie");
+					dogs.add("Mutt (Mixed Dog)");
+
+			}
+
+	}
