33d1bc665a7b4c9d348eb8a30f90aa6ef55cea2e
emdeline <emdeline@10.0.0.154>
2019-09-25 22:59:46 -0600

    Commit from home, all that is left is question 4 to code.

--- ArrayList Challenge #2/src/ArrayListChallenge2.java
+++ ArrayList Challenge #2/src/ArrayListChallenge2.java
@@ -6,6 +6,7 @@ public class ArrayListChallenge2
 
 		public static void main(String[] args)
 			{
+			//Question 1 code...
 				ArrayList <String> dogs = new ArrayList <String>();
 					dogs.add("Border Collie");
 					dogs.add("Blue Healer");
@@ -16,17 +17,33 @@ public class ArrayListChallenge2
 					dogs.add("Old English Shepherd");
 					dogs.add("Bernese Mountain Dog");
 					dogs.add("Corgie");
-					dogs.add("Mutt (Mixed Dog)");
+					dogs.add("Mutt");
 					
+			//Question 2 code...
 					Collections.sort(dogs);
 					
 					for (String d : dogs)
 						{
 							System.out.println(d);
 						}
+					
+					System.out.println(" ");
+					
+			//Question 3 code...
 					System.out.println(dogs.get(0));
 					System.out.println(dogs.get(9));
-
+					
+					System.out.println(" ");
+					
+			//Question 4 code...
+					for ()
+						{
+							System.out.println();
+						}
+					System.out.println(" ");
+					
+			//Question 5 code...
+					System.out.println("Mutt");
 						
 			}
 		
